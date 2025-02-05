ea42c9ec3412d463c6681e778ee01e889b3b33fc
seaofclouds <seaofclouds@users.noreply.github.com>
2025-02-04 16:56:02 -0800

    prep for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [react()],
   output: 'server',  // Use server output to handle dynamic routes
+  adapter: cloudflare(),
   trailingSlash: 'never'
 });
